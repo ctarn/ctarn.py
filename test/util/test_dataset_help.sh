5107d0d05bbc1786095b4b684541f0238a581974
Tarn Yeong Ching <i@ctarn.io>
2019-12-21 23:33:38 +0800

    /: add Reductive Clustering

---
+++ test/util/test_dataset_help.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./ctarn/util/dataset.py -h
+./ctarn/util/dataset.py get -h
+./ctarn/util/dataset.py sample -h
+./ctarn/util/dataset.py graph -h
+./ctarn/util/dataset.py reduce -h
+./ctarn/util/dataset.py show -h
